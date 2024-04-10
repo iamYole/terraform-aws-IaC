455e38d7c97e92a7045ce0d68f6301c3009141de
Gideon <gideonovuzorie@gmail.com>
2024-04-10 18:55:28 +0100

    ..

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,17 @@
 terraform {
-  backend "s3" {
+  /* backend "s3" {
     bucket         = "ytech-terraform-state"
     key            = "ytech/terraform-cloud/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
+  } */
+
+  backend "remote" {
+
+    organization = "YoleTechSolutions"
+    workspaces {
+      name = "terraform-aws-IaC"
+    }
   }
 }
